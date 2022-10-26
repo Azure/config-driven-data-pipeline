945c996dc176b7befcccd788492ccff90c92562d
maye-msft <maye-msft@outlook.com>
2022-10-26 14:05:13 +0800

    add streaming job assertion

--- tests/test_streaming.py
+++ tests/test_streaming.py
@@ -10,6 +10,7 @@ def create_spark():
 def test_example_pipeline_fruit_streaming(create_spark):
     serving_df = cddp.run_pipeline(create_spark, './example/pipeline_fruit_streaming.json', './tmp', None, None, True, True, 60, True)
     assert len(serving_df)== 1
-    list = serving_df[0].toPandas().sort_values(by='fruit', ascending=True).to_records().tolist()
+    list = serving_df[0].toPandas()[['fruit','total']].sort_values(by='fruit', ascending=True).to_records().tolist()
     assert len(list)==7
-    assert "[(3, 1, 'Red Grape', 24.0), (6, 2, 'Peach', 39.0), (4, 3, 'Orange', 28.0), (0, 4, 'Green Apple', 45.0), (2, 5, 'Fiji Apple', 56.0), (5, 6, 'Banana', 17.0), (1, 7, 'Green Grape', 36.0)]" == str(list)
+    assert "[(5, 'Banana', 17.0), (2, 'Fiji Apple', 56.0), (1, 'Green Apple', 45.0), (0, 'Green Grape', 36.0), (4, 'Orange', 28.0), (6, 'Peach', 39.0), (3, 'Red Grape', 24.0)]" == str(list)
+            
