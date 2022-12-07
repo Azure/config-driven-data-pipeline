0786e02c404d97446d0a37a2e5540c77802e4863
Bo Wang <36917625+Nick287@users.noreply.github.com>
2022-12-07 06:56:05 +0000

    quick fix ADLS_g_2 and add ADLS config json

--- src/cddp/ingestion/azure_adls_gen2.py
+++ src/cddp/ingestion/azure_adls_gen2.py
@@ -1,21 +1,22 @@
-
 from pyspark.sql.types import *
+import IPython
 
 def start_ingestion_task(task, spark):
-    import dbutils
+    # import dbutils
+    dbutils = IPython.get_ipython().user_ns["dbutils"]
     schema = StructType.fromJson(task["schema"])
-    storage_account = task["storage-account"]
-    if "sas-token" in task:
-        sas_token = dbutils.secrets.get(scope=task["secret_scope"], key=task["storage-account"])
+    storage_account = task['input']["storage_account"]
+    if "sas-token" in task['input']:
+        sas_token = dbutils.secrets.get(scope=task['input']["secret_scope"], key=task['input']["storage_account"])
         spark.conf.set(f"fs.azure.account.auth.type.{storage_account}.dfs.core.windows.net", "SAS")
         spark.conf.set(f"fs.azure.sas.token.provider.type.{storage_account}.dfs.core.windows.net", "org.apache.hadoop.fs.azurebfs.sas.FixedSASTokenProvider")
         spark.conf.set(f"fs.azure.sas.fixed.token.{storage_account}.dfs.core.windows.net", sas_token)
 
-    elif "service-credential-key" in task:
+    elif "service_credential_key" in task['input']:
 
-        application_id= dbutils.secrets.get(scope=task["secret_scope"], key=task["application_id"])
-        directory_id= dbutils.secrets.get(scope=task["secret_scope"], key=task["directory_id"])
-        service_credential = dbutils.secrets.get(scope=task["secret_scope"],key=task["service_credential_key"])
+        application_id= dbutils.secrets.get(scope=task['input']["secret_scope"], key=task['input']["application_id"])
+        directory_id= dbutils.secrets.get(scope=task['input']["secret_scope"], key=task['input']["directory_id"])
+        service_credential = dbutils.secrets.get(scope=task['input']["secret_scope"],key=task['input']["service_credential_key"])
 
         spark.conf.set(f"fs.azure.account.auth.type.{storage_account}.dfs.core.windows.net", "OAuth")
         spark.conf.set(f"fs.azure.account.oauth.provider.type.{storage_account}.dfs.core.windows.net", "org.apache.hadoop.fs.azurebfs.oauth2.ClientCredsTokenProvider")
@@ -23,16 +24,16 @@ def start_ingestion_task(task, spark):
         spark.conf.set(f"fs.azure.account.oauth2.client.secret.{storage_account}.dfs.core.windows.net", service_credential)
         spark.conf.set(f"fs.azure.account.oauth2.client.endpoint.{storage_account}.dfs.core.windows.net", f"https://login.microsoftonline.com/{directory_id}/oauth2/token")
 
-    elif "storage-account-access-key" in task:
-        storage_account_access_key = dbutils.secrets.get(scope=task["secret_scope"],key="storage-account-access-key")        
+    elif "storage_account_access_key" in task['input']:
+        storage_account_access_key = dbutils.secrets.get(scope=task['input']["secret_scope"],key="storage_account_access_key")        
         spark.conf.set(f"fs.azure.account.key.{storage_account}.dfs.core.windows.net", storage_account_access_key)
 
-    container_name = task["container-name"]
-    path_to_data = task["data-folder"]
+    container_name = task['input']["container_name"]
+    path_to_data = task['input']["data_folder"]
     path = f"abfss://{container_name}@{storage_account}.dfs.core.windows.net/{path_to_data}"
 
     # read data of parquet, JSON, CSV, Text
-    df = spark.read.format(task["format"]) \
+    df = spark.read.format(task['input']["format"]) \
         .option("header", "true") \
         .schema(schema) \
         .load(path)
