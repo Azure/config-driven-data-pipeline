9234e5032cbb4dd7a0db023b3bf7d11e04feead7
Bo Wang <36917625+Nick287@users.noreply.github.com>
2023-03-22 10:30:28 +0000

    test

--- src/cddp/ingestion/azure_adls_adf_syn.py
+++ src/cddp/ingestion/azure_adls_adf_syn.py
@@ -3,13 +3,14 @@ from pyspark.sql.types import *
 from datetime import datetime, timedelta
 import time
 import datetime
+
 from azure.identity import ClientSecretCredential
 from azure.mgmt.resource import ResourceManagementClient
 from azure.mgmt.datafactory import DataFactoryManagementClient
 from azure.mgmt.datafactory.models import *
 
 
-def start_ingestion_adf(task, spark):
+def start_ingestion_task(task, spark):
     from notebookutils import mssparkutils
 
     now = datetime.datetime.now()
@@ -60,14 +61,14 @@ def start_ingestion_adf(task, spark):
 
     schema = StructType.fromJson(task["schema"])
     storage_account = task['input']["storage_account"]
-    if "sas-token" in task:
+
+    if "sas-token" in task['input']:
         sas_token = mssparkutils.credentials.getSecret(task['input']["secret_scope"], task['input']["storage_account"])
         spark.conf.set(f"fs.azure.account.auth.type.{storage_account}.dfs.core.windows.net", "SAS")
         spark.conf.set(f"fs.azure.sas.token.provider.type.{storage_account}.dfs.core.windows.net", "org.apache.hadoop.fs.azurebfs.sas.FixedSASTokenProvider")
         spark.conf.set(f"fs.azure.sas.fixed.token.{storage_account}.dfs.core.windows.net", sas_token)
 
-    elif "service-credential-key" in task:
-
+    elif "service_credential_key" in task['input']:
         application_id = mssparkutils.credentials.getSecret(task['input']["secret_scope"], task['input']["application_id"]) 
         directory_id = mssparkutils.credentials.getSecret(task['input']["secret_scope"], task['input']["directory_id"]) 
         service_credential = mssparkutils.credentials.getSecret(task['input']["secret_scope"],task['input']["service_credential_key"])
@@ -77,9 +78,9 @@ def start_ingestion_adf(task, spark):
         spark.conf.set(f"fs.azure.account.oauth2.client.id.{storage_account}.dfs.core.windows.net", application_id)
         spark.conf.set(f"fs.azure.account.oauth2.client.secret.{storage_account}.dfs.core.windows.net", service_credential)
         spark.conf.set(f"fs.azure.account.oauth2.client.endpoint.{storage_account}.dfs.core.windows.net", f"https://login.microsoftonline.com/{directory_id}/oauth2/token")
-    
-    elif "storage_account-access-key" in task:
-        storage_account_access_key = mssparkutils.credentials.getSecret(task['input']["secret_scope"], "storage-account-access-key") 
+
+    elif "storage_account_access_key" in task['input']:
+        storage_account_access_key = mssparkutils.credentials.getSecret(scope=task['input']["secret_scope"],key="storage_account_access_key")        
         spark.conf.set(f"fs.azure.account.key.{storage_account}.dfs.core.windows.net", storage_account_access_key)
 
     container_name = task['input']["container_name"]
@@ -87,7 +88,7 @@ def start_ingestion_adf(task, spark):
     path = f"abfss://{container_name}@{storage_account}.dfs.core.windows.net/{path_to_data}"
 
     # read data of parquet, JSON, CSV, Text
-    df = spark.read.format(task["input"]["format"]) \
+    df = spark.read.format(task['input']["format"]) \
         .option("header", "true") \
         .schema(schema) \
         .load(path)
