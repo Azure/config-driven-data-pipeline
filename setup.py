a1f677175277c2df587b670ec18f0ae39cd74202
Bo Wang <36917625+Nick287@users.noreply.github.com>
2023-03-22 10:25:45 +0000

    test

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="cddp",
-    version="0.0.9",
+    version="0.1.9",
     author="Sean Ma",
     author_email="maye-msft@outlook.com",
     description="Config Driven Data Pipeline",
